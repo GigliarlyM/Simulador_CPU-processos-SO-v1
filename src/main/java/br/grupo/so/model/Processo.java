048dc34cb9f9fbe6a897cb3fb8c8d8fec0f649ec
Luiz Neto <138041202+CtrlNeto@users.noreply.github.com>
2025-09-04 21:01:27 -0300

    feat: Lógica da classe de Processo

--- src/main/java/br/grupo/so/model/Processo.java
+++ src/main/java/br/grupo/so/model/Processo.java
@@ -4,6 +4,38 @@ public class Processo implements Comparable<Processo>{
   int id;
   int quantidadeInstrucoes;
 
+  public Processo(int id, int quantidadeInstrucoes) {
+    this.id = id;
+    this.quantidadeInstrucoes = quantidadeInstrucoes;
+  }
+
+  public void executarInstrucao() {
+    if(quantidadeInstrucoes > 0) {
+      quantidadeInstrucoes--;
+      System.out.println("O processo de " + id + " foi executado. Restam " + quantidadeInstrucoes + " processos");
+    }
+
+    else {
+      System.out.println("Os processos acabaram. Processo finalizado");
+    }
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getQuantidadeInstrucoes() {
+    return quantidadeInstrucoes;
+  }
+
+  public void setQuantidadeInstrucoes(int quantidadeInstrucoes) {
+    this.quantidadeInstrucoes = quantidadeInstrucoes;
+  }
+
   @Override
   public int compareTo(Processo outro) {
     return Integer.compare(quantidadeInstrucoes, outro.quantidadeInstrucoes);
