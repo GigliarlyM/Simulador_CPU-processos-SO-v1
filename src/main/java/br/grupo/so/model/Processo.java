15a04f6710a507e447c661326fe53cba2463fd01
Nicollas-Samuel <nicollassamueldasilvabarbosan@gmail.com>
2025-09-07 20:11:55 -0300

    add func algor FCFS

--- src/main/java/br/grupo/so/model/Processo.java
+++ src/main/java/br/grupo/so/model/Processo.java
@@ -1,48 +1,73 @@
 package br.grupo.so.model;
 
-public class Processo implements Comparable<Processo>{
-  int id;
-  int quantidadeInstrucoes;
+public class Processo {
+    public String id;
+    private int chegada;
+    private int burst;
+    private int inicio;
+    private int termino;
+    private int turnaround;
+    private int espera;
+	public int quantidadeInstrucoes;
 
-  public Processo(int id, int quantidadeInstrucoes) {
-    this.id = id;
-    this.quantidadeInstrucoes = quantidadeInstrucoes;
-  };
-
-  public void executarInstrucao() {
-    if(quantidadeInstrucoes > 0) {
-      quantidadeInstrucoes--;
-      System.out.println("O processo de " + id + " foi executado. Restam " + quantidadeInstrucoes + " processos");
+    // Construtor
+    public Processo(String id, int chegada, int burst) {
+        this.id = id;
+        this.chegada = chegada;
+        this.burst = burst;
+        this.inicio = 0;
+        this.termino = 0;
+        this.turnaround = 0;
+        this.espera = 0;
     }
 
-    else {
-      System.out.println("Os processos acabaram. Processo finalizado");
-    };
-  };
+    public Processo(int i, int quantidadeInstrucoes, int chegada2, int burst2) {
+		// TODO Auto-generated constructor stub
+	}
 
-  public int getId() {
-    return id;
-  };
+	// Getters
+    public String getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  };
+    public int getChegada() {
+        return chegada;
+    }
 
-  public int getQuantidadeInstrucoes() {
-    return quantidadeInstrucoes;
-  };
+    public int getBurst() {
+        return burst;
+    }
 
-  public void setQuantidadeInstrucoes(int quantidadeInstrucoes) {
-    this.quantidadeInstrucoes = quantidadeInstrucoes;
-  };
+    public int getInicio() {
+        return inicio;
+    }
 
-  @Override
-  public int compareTo(Processo outro) {
-    return Integer.compare(quantidadeInstrucoes, outro.quantidadeInstrucoes);
-  }
+    public int getTermino() {
+        return termino;
+    }
 
-  @Override
-  public String toString() {
-	return "Processo [id=" + id + ", quantidadeInstrucoes=" + quantidadeInstrucoes + "]";
-  };
-};
+    public int getTurnaround() {
+        return turnaround;
+    }
+
+    public int getEspera() {
+        return espera;
+    }
+
+    // Setters
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setTermino(int termino) {
+        this.termino = termino;
+    }
+
+    public void setTurnaround(int turnaround) {
+        this.turnaround = turnaround;
+    }
+
+    public void setEspera(int espera) {
+        this.espera = espera;
+    }
+}
--- src/main/java/br/grupo/so/model/escalonador/FCFS.java
+++ src/main/java/br/grupo/so/model/escalonador/FCFS.java
@@ -6,30 +6,58 @@ import br.grupo.so.model.Processo;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+import java.util.*;
+
 public class FCFS extends Escalonador {
-  Queue<Processo> processos;
+	Queue<Processo> processos;
 
-  public FCFS() {
-    this.processos = new ArrayDeque<Processo>();
-  };
+	public FCFS() {
+		this.processos = new ArrayDeque<Processo>();
+	};
 
-  @Override
-  public void adicionarProcesso(Processo processo) {
-    this.processos.offer(processo);
-  };
+	@Override
+	public void adicionarProcesso(Processo processo) {
+		this.processos.offer(processo);
+	};
 
-  @Override
-  public Processo proximoProcesso() {
-    return this.processos.poll();
-  };
+	@Override
+	public Processo proximoProcesso() {
+		return this.processos.poll();
+	};
 
-  @Override
-  public void removerProcesso(Processo processo) {
-    this.processos.remove(processo);
-  };
+	@Override
+	public void removerProcesso(Processo processo) {
+		this.processos.remove(processo);
+	};
 
-  @Override
-  public boolean temProcesso() {
-    return !this.processos.isEmpty();
-  };
+	@Override
+	public boolean temProcesso() {
+		return !this.processos.isEmpty();
+	};
+
+	public void simular() {
+	    List<Processo> lista = new ArrayList<>(processos);
+	    lista.sort(Comparator.comparingInt(Processo::getChegada));
+
+	    int tempoAtual = 0;
+	    for (Processo p : lista) {
+	        if (tempoAtual < p.getChegada()) {
+	            tempoAtual = p.getChegada();
+	        }
+
+	        p.setInicio(tempoAtual);
+	        p.setTermino(p.getInicio() + p.getBurst());
+	        p.setTurnaround(p.getTermino() - p.getChegada());
+	        p.setEspera(p.getTurnaround() - p.getBurst());
+
+	        tempoAtual = p.getTermino();
+	    }
+
+	    System.out.println("ID | Chegada | Burst | Início | Término | Turnaround | Espera");
+	    for (Processo p : lista) {
+	        System.out.printf("%s  |   %d      |   %d   |   %d     |   %d      |     %d      |   %d\n",
+	                p.getId(), p.getChegada(), p.getBurst(),
+	                p.getInicio(), p.getTermino(), p.getTurnaround(), p.getEspera());
+	    };
+	};
 };
